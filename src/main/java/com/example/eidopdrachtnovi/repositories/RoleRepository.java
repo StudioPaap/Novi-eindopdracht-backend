ba42ae341755657589b4ac7d6f99a19c953518df
StudioPaap <anoukpaap@gmail.com>
2023-04-30 20:52:18 +0200

    afronden

--- src/main/java/com/example/eidopdrachtnovi/repositories/RoleRepository.java
+++ src/main/java/com/example/eidopdrachtnovi/repositories/RoleRepository.java
@@ -1,8 +1,10 @@
 package com.example.eidopdrachtnovi.repositories;
 
+import com.example.eidopdrachtnovi.models.Project;
 import com.example.eidopdrachtnovi.models.Role;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 
 
 public interface RoleRepository extends CrudRepository<Role, String> {
